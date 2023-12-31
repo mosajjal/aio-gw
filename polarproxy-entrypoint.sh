34319fb8861ca24e58a01e19b7c1ee2c648bdd79
Ali Mosajjal <hi@n0p.me>
2024-01-01 11:35:35 +1300

    added tcpdump

--- polarproxy-entrypoint.sh
+++ polarproxy-entrypoint.sh
@@ -7,12 +7,15 @@ set -e
 ip tuntap add mode tun dev tun0 && \
 ip addr add 100.100.100.100/32 dev tun0 && \
 ip link set dev tun0 up
-# TODO: egress is a hardcoded container name that needs to be an env variable
+# TODO: egress is a HARDCODED container name that needs to be an env variable
 opt/tun2socks/tun2socks-linux-amd64 -device tun0 -proxy socks5://egress:1080 &
 # remove default route and replace with tun0
 
 sh -c "sleep 30 && ip route del default && ip route add default dev tun0 && sleep 1" &
 
+# run tcpdump to capture all traffic from tun0 to arkime
+# TODO: arkime is HARDCODED
+tcpdump -i tun0 -w - | nc arkime 57012 &
 
 cd /opt/polarproxy
 dotnet PolarProxy.dll $@
